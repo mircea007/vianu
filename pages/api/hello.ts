84b7cd61acb2a6553453e5bc8be8e99029ecdbea
mircea007 <mirceareb@gmail.com>
2022-12-09 23:09:40 +0200

    simple database api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+const { Client } = require('pg')
+ 
+const client = new Client({
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  password: process.env.PGPASSWORD,
+  user: process.env.PGUSER,
+})
 
 type Data = {
   name: string
@@ -9,5 +17,21 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  client.connect().then( _ => {
+    client.query( "SELECT * FROM users WHERE id=1", (qerr, qres) => {
+      if( qerr ){
+        res.status( 500 ).send( 'Database error' )
+        throw qerr
+      }
+
+      client.end(); // close database connection
+
+      res.status( 200 ).json( qres.rows )
+    });
+
+  }).catch( err => { // connect error
+    console.log( err.stack )
+    res.status( 500 )
+  })
 }
