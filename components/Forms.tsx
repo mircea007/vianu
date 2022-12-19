01a3d7b6e277835ab83542c930d9e6d235173074
raresferaru <64482832+raresferaru@users.noreply.github.com>
2022-12-19 22:08:28 +0200

    Add files via upload
    Added flash message for submit btn

--- components/Forms.tsx
+++ components/Forms.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 export function InputField(
   { type, value, onChange, comment, label, placeholder, className }:
-  { type: string, value: string, onChange: (evt: React.ChangeEvent<HTMLInputElement>) => void, comment?: string, label: string, placeholder: string, className?: string }
-){
+    { type: string, value: string, onChange: (evt: React.ChangeEvent<HTMLInputElement>) => void, comment?: string, label: string, placeholder: string, className?: string }
+) {
 
   let error = 1;
-  if( !comment ){
+  if (!comment) {
     error = 0;
     comment = '\u200B'
   }
@@ -15,11 +17,11 @@ export function InputField(
         <span className="text-lg font-bold mb-2">{label}</span>
         <br />
         <input
-        className={"transition transition-all duration-300 float-right px-2 py-1 outline outline-2 rounded-lg " + (error ? "outline-red-300 focus:outline-red-400" : "outline-gray-300 focus:outline-main-300")}
-        type={type}
-        value={value}
-        placeholder={placeholder || ""}
-        onChange={onChange}
+          className={"transition transition-all duration-300 float-right px-2 py-1 outline outline-2 rounded-lg " + (error ? "outline-red-300 focus:outline-red-400" : "outline-gray-300 focus:outline-main-300")}
+          type={type}
+          value={value}
+          placeholder={placeholder || ""}
+          onChange={onChange}
         />
       </div> <br />
       <span className="test-md h-4 text-red-500">{comment || ""}</span>
@@ -27,13 +29,71 @@ export function InputField(
   )
 }
 
-export function SubmitButton({ value, disabled }: { value?: string, disabled?: boolean }){
+export function SubmitButton({ message, type, value, disabled }: { message?: string, type?: string, value?: string, disabled?: boolean }) {
+  const [isAlertVisible, setIsAlertVisible] = React.useState(false);
+
+  const handleButtonClick = () => {
+    setIsAlertVisible(true);
+  }
+
+  setTimeout(() => {
+    setIsAlertVisible(false);
+  }, 4000);
+  const styles = {
+    alertContainer: {
+      position: 'absolute' as 'absolute',
+      top: '2rem',
+      left: '0',
+      right: '0'
+    },
+
+    alertInner: {
+      position: 'absolute' as 'absolute',
+      top: '60px',
+      display: 'inline-block',
+      padding: '8px 16px',
+      zIndex: '1',
+      color: '#fff',
+      minWidth: '300px',
+      borderRadius: '5px',
+      //backgroundColor: '#ffffff',
+      //margin: '0 auto',
+      right: '2rem',
+      boxShadow: '1px 2px 10px - 3px rgb(0 0 0 / 70 %)',
+      backgroundColor: '#45d49d'
+
+    }
+  }
+
+  if (type === 'wrong') {
+    styles.alertInner.backgroundColor = 'red'
+  } else if (type === 'warning') {
+    styles.alertInner.backgroundColor = 'yellow'
+  } else if (type === 'info') {
+    styles.alertInner.backgroundColor = 'blue'
+  }
+
+
   return (
-    <input
-    className="font-bold uppercase bg-main-500 text-white p-2 rounded-lg hover:bg-main-600 cursor-pointer focus:outline focus:outline-2 focus:outline-main-300 disabled:bg-gray-300 disabled:text-gray-200 disabled:outline-0"
-    type="submit"
-    value={value ? value : "Submit"}
-    disabled={disabled || false}
-    />
+    <div>
+      <input
+        onClick={handleButtonClick}
+        className="font-bold uppercase bg-main-500 text-white p-2 rounded-lg hover:bg-main-600 cursor-pointer focus:outline focus:outline-2 focus:outline-main-300 disabled:bg-gray-300 disabled:text-gray-200 disabled:outline-0"
+        type="submit"
+        value={value ? value : "Submit"}
+        disabled={disabled || false}
+      />
+      {
+        isAlertVisible && <div style={styles.alertContainer}>
+          <div style={styles.alertInner}>{message}</div>
+        </div>
+      }
+
+    </div>
+
+
+
+
+
   )
 }
